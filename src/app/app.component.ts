42f72e3f267c8db6ed8869981c8ce3d8315fe034
Kompiman <rizkykompiman@gmail.com>
2019-08-08 14:55:12 +0700

    tst

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import * as jsPDF from 'jspdf';
+import { $ } from 'protractor';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test';
+  title = 'cobapdf';
+  
+  public name = "";
+  public dob = "";
+  public address = "";
+
+  downloadPDF(){
+    console.log('downloading pdf...');
+    console.log(`${this.name}`);
+    console.log(`${this.dob}`);
+    console.log(`${this.address}`);
+    
+    const imgData ='C:\Users\User-PC\test\src\assets\img\download.jpg'
+    const doc = new jsPDF();
+    
+    doc.setFont('times')
+    doc.setFontSize(10)
+
+    doc.addImage(imgData, 'jpg', 15, 20, 10, 35)
+    doc.text(`The name that your parent choose = ${this.name}`, 15, 15);
+    doc.text(`The date you born = ${this.dob}`, 15, 35);
+    doc.text(`Where do you live = ${this.address}`, 15, 55);
+    
+    doc.save('first.pdf');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
